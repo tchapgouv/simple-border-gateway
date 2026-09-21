881c91b33e62e5786296c5d0239be7983f6d2446
Mathieu Velten <mathieu.velten@numerique.gouv.fr>
2026-09-21 11:28:03 +0200

    Fix upstream proxy auth logic
    if only one was set it would just disregard the other

--- src/util.rs
+++ src/util.rs
@@ -385,10 +385,12 @@ pub fn create_http_client(
     if let Some(upstream_proxy_config) = upstream_proxy_config {
         let mut proxy_builder = reqwest::Proxy::all(upstream_proxy_config.url)
             .whatever_context("Failed to create reqwest proxy config")?;
-        if let Some(username) = &upstream_proxy_config.username
-            && let Some(password) = &upstream_proxy_config.password
+        if upstream_proxy_config.username.is_some() || upstream_proxy_config.password.is_some()
         {
-            proxy_builder = proxy_builder.basic_auth(username, password);
+            proxy_builder = proxy_builder.basic_auth(
+                upstream_proxy_config.username.as_deref().unwrap_or(""),
+                upstream_proxy_config.password.as_deref().unwrap_or(""),
+            );
         }
         builder = builder.proxy(proxy_builder);
     }
